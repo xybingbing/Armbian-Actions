be23278607945f30c43c6593fe19a3aba0ff5f9d
xuyang <xuyang@xuyangdeMac-mini.local>
2025-04-26 15:11:19 +0800

    fix

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "安装环境"
+sudo apt update
+sudo apt install -y git gnupg flex bison build-essential zip curl zlib1g-dev \
+libc6-dev-i386 libncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev \
+libgl1-mesa-dev libxml2-utils xsltproc unzip bc python3 python3-distutils
+echo "--------------------------------"
+sudo rm -rf /data
+sudo mkdir -p /data
+sudo chown $USER:$GROUPS /data
+sudo rm -rf workdir
+sudo mkdir -p workdir
+sudo chown $USER:$GROUPS workdir
+cd workdir
+WORKDIR=$(pwd)
+echo "获取armbian源码"
+#获取armbian
+git clone --depth=1 https://github.com/armbian/build build
+ln -sf $WORKDIR/build /data/build
+cd /data/build
+# 复制 config 和 userpatches 目录文件
+cp -rf ${WORKDIR}/../addboard/config/* config
+mkdir -p userpatches
+cp -rf ${WORKDIR}/../addboard/userpatches/* userpatches
+ls -la
+echo "--------------------------------"
+echo "编译镜像"
+./compile.sh build BOARD=tpm312 BRANCH=stable BUILD_DESKTOP=no BUILD_MINIMAL=yes COMPRESS_OUTPUTIMAGE=sha,xz KERNEL_CONFIGURE=no RELEASE=jammy
+
+
+
