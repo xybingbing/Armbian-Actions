cc77721b05e6a372d8881208361593858c0f67d5
xybingbing <xybingbing@live.com>
2025-04-26 19:54:46 +0800

    fix

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ cp -rf ${WORKDIR}/../addboard/userpatches/* userpatches
 ls -la
 echo "--------------------------------"
 echo "编译镜像"
-./compile.sh build BOARD=tpm312 BRANCH=stable BUILD_DESKTOP=no BUILD_MINIMAL=yes COMPRESS_OUTPUTIMAGE=sha,xz KERNEL_CONFIGURE=no RELEASE=jammy
+./compile.sh build BOARD=edge-force BRANCH=stable BUILD_DESKTOP=no BUILD_MINIMAL=yes COMPRESS_OUTPUTIMAGE=sha,xz KERNEL_CONFIGURE=no RELEASE=jammy
 
 
 
